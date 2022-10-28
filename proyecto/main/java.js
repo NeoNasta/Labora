8d46b0899eb138e1c7b37bcfffcb4c5d0b54c7bf
NeoNasta <87004011+NeoNasta@users.noreply.github.com>
2022-10-28 20:45:30 -0300

    creando calculador de precios
    se agrego el calculador de precios

---
+++ proyecto/main/java.js
@@ -0,0 +1,18 @@
+var costs = {
+	clases: {basePrice: 250, maxClass: 12},
+    meses: {basePrice: 3500, maxClass: 12}
+};
+  
+  function clasesCalculate()
+  {
+    var clases = document.getElementById('clases').value;
+    var clasexprice = costs.clases.basePrice * clases;
+    document.getElementById('clasexprice').innerHTML = "$" + clasexprice;
+  }
+
+  function mesesCalculate()
+  {
+    var meses = document.getElementById('meses').value;
+    var mesesprice = costs.meses.basePrice * meses;
+    document.getElementById('mesesprice').innerHTML = "$" + mesesprice;
+  }
